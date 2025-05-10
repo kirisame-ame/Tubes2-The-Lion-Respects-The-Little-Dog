c2f87e4bed8481ca422d1a55044ba5835b5210a3
William Andrian Dharma T <williamadt123@gmail.com>
2025-05-10 17:16:24 +0700

    fix: Removed Unused Imports

--- app/frontend/src/hooks/FlowContext.tsx
+++ app/frontend/src/hooks/FlowContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, ReactNode } from "react";
-import { ReactFlowInstance, Node, Edge } from "@xyflow/react";
+import { ReactFlowInstance, Node } from "@xyflow/react";
 
 type FlowContextType = {
     reactFlowInstance: ReactFlowInstance | null;
